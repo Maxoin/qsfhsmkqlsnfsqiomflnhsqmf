a49980346e3724dec5ea639370c0d1b544d911a7
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-03-03 11:47:42 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ var bot = new Discord.Client();
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
  
-const adapter = new FileSync('master/.gitignore/db.js')
+const adapter = new FileSync('.gitignore/db.js')
 const db = low(adapter)
 
 const adapterp = new FileSync('pkmn.json')
