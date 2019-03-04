eff337bb035466f5eb53af82b5375eb735fdedf1
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-03-04 16:52:32 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -178,6 +178,103 @@ var gifs = {
     }
 }
 
+//Declaration Variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var channelStockId = "552143842309046272";  //Max, met ici l'id du channel !
+
+var dataBank = []; //Contient des tableaux : C'est la base de données quand le bot est actif !
+
+
+
+//Declaration Fonction~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+function read () { //lit le stockage discord et le met dans le stockage variable
+
+    // met le contenu des messages dans dataBank
+        (bot.channels.get(channelStockId).fetchMessages({ limit: 100 }) 
+            .then(messages => 
+               
+                messages.forEach(function(valeur , clé) {
+                    dataBank.push(valeur.content.split(" * "));
+                })
+            )
+            .catch(console.error)
+        );
+    
+};
+
+function postReserve (id) { //poste un message dans l'espace de stockage discord ( en théorie, l'id des utilisateurs ayant fait !!OwOLog)
+    bot.channels.get(channelStockId).send(id);
+    
+}
+
+function findUser (id) { //à partir de l'id d'un user, trouve l'indice de sa "fiche" dans le stockage => -1 si pas de fiche
+    var userIndice;
+    dataBank.forEach(function(valeur, clé) {
+        if (id ===  valeur[0]) {
+            userIndice = clé;
+        }
+    })
+    if (userIndice == undefined) {
+        return -1;
+    } else { 
+        return userIndice;
+    }
+}
+
+function add (id, numOwO) { //Ajoute un OwO à un user
+    if (findUser(id) != -1) {    
+
+        
+
+        bot.channels.get(channelStockId).fetchMessages({ limit: 100 }) //Trouve le message de stockage discord de l'user puis l'edit pour ajouter le owo
+
+            .then(messages => 
+                messages.forEach(function(msg, idMsg) {
+                    
+                    if (msg.content.split(' * ')[0] == id) {
+                        bot.channels.get(channelStockId).fetchMessage(idMsg)
+                            .then(message => 
+                                message.edit(msg.content + " * " + numOwO)
+                            
+                            )
+                            .catch(console.error)
+                    }
+                })
+            )
+            .catch(console.error)
+
+        dataBank[findUser(id)].push(numOwO); //Ajoute le owo dans le stockage variable
+    
+
+    }
+
+}
+
+function gotOwO (id, numOwO) { //Verifie si un user a un OwO ! return true si il l'a, return false sinon
+
+
+    var got = false;
+    if (findUser(id) != -1 ) {
+
+        dataBank[findUser(id)].forEach(function (valeur) {
+
+
+            if (valeur === numOwO) {
+                got = true;
+            } 
+            
+        });
+    
+    return got;
+
+    }
+
+
+}
+
 bot.on('ready',() => {
     console.log('Bot Ready')
 })
@@ -185,8 +282,6 @@ bot.on('ready',() => {
 bot.login (process.env.token);
 
 // Set some defaults
-db.defaults({ stats: {}})
-  .write()
 dbp.defaults({ mowo: {}})
   .write()
 
