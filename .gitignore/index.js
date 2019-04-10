7bc45318d7a0d3711a74024c491c133108227e88
Maxoin <39875172+Maxoin@users.noreply.github.com>
2019-04-10 18:54:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -130,7 +130,7 @@ bot.on('message', message => { //Log
   })
 
 bot.on('message', message => {
-    if(msg.content.split(': ')[0] === repereInteraction.noms){
+    if(message.content.split(': ')[0] === repereInteraction.noms){
         message.channel.send("ça marche '^'")
     }
 })
